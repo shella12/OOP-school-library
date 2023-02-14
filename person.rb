8bdcb5617c656626b82eae19e3977261b306d7cd
shella12 <ayeshaarshad4567@gmail.com>
2023-02-14 16:34:29 +0500

    Implement changes

--- person.rb
+++ person.rb
@@ -1,16 +1,16 @@
 class Person
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
-    @parent_permission = parent_permission
+    @parent_permission ||= parent_permission
   end
 
   def can_use_services?
-    is_of_age? or @parent_permission
+    of_age? or @parent_permission
   end
 
   private
@@ -19,3 +19,6 @@ class Person
     age >= 18
   end
 end
+
+p = Person.new(12, ' nm')
+puts p.can_use_services?

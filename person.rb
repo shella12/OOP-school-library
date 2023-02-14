cf2df433824dff7bd14e164bad2686f8ccfcb341
shella12 <ayeshaarshad4567@gmail.com>
2023-02-14 14:32:03 +0500

    Write class Person

--- person.rb
+++ person.rb
@@ -1,3 +1,19 @@
 class Person
+    attr_reader :id
+    attr_accessor :name, :age
+    def initialize (age, name="Unknown", parent_permission=true)
+        @id = Random.rand(1..100)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
 
+    def can_use_services?
+        is_of_age? or @parent_permission
+    end
+
+    private
+    def is_of_age?
+        self.age >= 18
+    end
 end

5095a18b03584bc4eb8f892e67687fad379533c6
shella12 <ayeshaarshad4567@gmail.com>
2023-02-23 08:01:27 +0500

    Fix linter errors

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -3,9 +3,8 @@ require './book'
 require './person'
 
 describe Rental do
-
   it 'should return list of rents by person' do
-    rental = Rental.new("2020/12/04", Book.new("title","author"), Person.new(23,"James"))
-    expect(rental.person.name).to eql ("James")
+    rental = Rental.new('2020/12/04', Book.new('title', 'author'), Person.new(23, 'James'))
+    expect(rental.person.name).to eql('James')
   end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,7 +1,6 @@
 require './teacher'
 
 describe Teacher do
-
   it 'should return true when we call can_use_services? method' do
     teacher = Teacher.new('Biology', 12)
     expect(teacher.can_use_services?).to be_truthy
